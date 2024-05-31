5e15a3373bd6bed5b510686ad4438eaf580d1414
Coen de Vente <cdvente@int4.local.snellius.surf.nl>
2024-05-31 14:57:17 +0200

    updated predict ubix farsiu folder

--- ubix/data/predict_ubix_farsiu.py
+++ ubix/data/predict_ubix_farsiu.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     os.environ["WANDB_SILENT"] = "true"
     
     parser = argparse.ArgumentParser()
-    parser.add_argument("--folder", default="Farsiu_NIFTI/resampled")
+    parser.add_argument("--folder", default="/path/to/nifti/resampled")
     args = parser.parse_args()
     
     ubix_type = "soft"
@@ -18,8 +18,13 @@ if __name__ == "__main__":
     # uncertainty_measure = "max_logit_max0th_confidence"
     uncertainty_measure = "ordinal_entropy"
 
-    model_name = "pretrained-maxpooling/0"
-    predictions_postfix = "_aug_10_mean"  # TTA
+    # model_name = "pretrained-maxpooling/0"
+    # predictions_postfix = "_aug_10_mean"  # TTA
+    
+    model_name = "pretrained-maxpooling"
+    predictions_postfix = "_deep_ensemble_5_mean"
+    
+    change_config = {"data_root": args.folder}
     
     save_path = f"./results/models/{model_name}/ubix_params_hval_{ubix_type}{predictions_postfix}_{uncertainty_measure}.p"
 
@@ -32,7 +37,7 @@ if __name__ == "__main__":
         model_name=model_name,
         model_type="best_quadratic_weighted_kappa",
         do_not_load_model=True,
-        change_config={"data_root": args.folder}
+        change_config=change_config
     )
     y_pred_ubix = ubix.predict(
         test_predictor, predict_set="test", predictions_postfix=predictions_postfix
@@ -43,13 +48,15 @@ if __name__ == "__main__":
     
     qwk = lambda a, b: sklearn.metrics.cohen_kappa_score(a, b, weights="quadratic")
     y_pred_ubix_merged = np.argmax(y_pred_ubix, -1)
-    y_pred_ubix_merged >= 2
+    y_pred_ubix_merged = (y_pred_ubix_merged >= 1).astype(int)
     
     y_pred_non_ubix_merged = np.argmax(predictions_non_ubix["y_pred"], -1)
-    y_pred_non_ubix_merged >= 2
+    y_pred_non_ubix_merged = (y_pred_ubix_merged >= 1).astype(int)
     
     qwk_ubix = qwk(predictions_non_ubix["y_true"], y_pred_ubix_merged)
-    qwk_non_ubix = qwk(predictions_non_ubix["y_true"], predictions_non_ubix["y_pred"])
+    qwk_non_ubix = qwk(predictions_non_ubix["y_true"], y_pred_non_ubix_merged)
     
     print("qwk_ubix:", qwk_ubix)
     print("qwk_non_ubix:", qwk_non_ubix)
+
+    pass
